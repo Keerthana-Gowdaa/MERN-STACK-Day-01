cbd27986c1d798c65a570356826a9cecb0e50b03
Keerthana-Gowdaa <keerthana.gowdaa66@gmail.com>
2026-02-27 11:44:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
 });
 
 /* SERVER PORT */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
